f39e4dc1fbfae3dd4ba82e344adf3b5f313ec00d
Iftisam-sandik <sandik55575@gmail.com>
2025-03-18 23:52:26 +0600

    first commit for todoApp

---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,34 @@
+import styles from "./todoitem.module.css";
+export default function TodoItem({ item, todos, setTodos }) {
+  function handleDelete(item) {
+    setTodos(todos.filter((todo) => todo !== item));
+  }
+  function handleClick() {
+    console.log("item text clicked");
+  }
+
+  return (
+    <div className={styles.item}>
+      <div className={styles.itemName}>
+        <span
+          onClick={() => {
+            handleClick;
+          }}
+        >
+          {item.name}
+        </span>
+
+        <span>
+          <button
+            onClick={() => handleDelete(item)}
+            className={styles.deleteButton}
+          >
+            x
+          </button>
+        </span>
+      </div>
+
+      <hr className={styles.line} />
+    </div>
+  );
+}
